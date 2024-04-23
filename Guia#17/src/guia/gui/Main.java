f53fd84a1b96529dab1a54d13bb4e469b84f723c
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-23 15:29:17 -0500

    Centralizar codigo

---
+++ Guia#17/src/guia/gui/Main.java
@@ -0,0 +1,22 @@
+
+package guia.gui;
+
+import guia.gui.Window;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+    
+public static void main(String[]args){
+    
+Window ventana = new Window();
+
+ventana.setLocationRelativeTo(null);
+ventana.setVisible(true);
+
+
+}
+    
+}
